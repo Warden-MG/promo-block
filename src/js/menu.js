cb7bf0d81b0c03ededc5ac371a45a775eb9d57eb
Warden-MG <zudilin-anton@mail.ru>
2026-04-13 22:39:37 +0600

    feat: Преднастройка общих стилей проекта

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,9 @@
-const burger = document.querySelector('.burger');
+const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.header__menu');
 
 if (burger && menu) {
   burger.addEventListener('click', () => {
-    burger.classList.toggle('burger--active');
+    burger.classList.toggle('header__burger--active');
     menu.classList.toggle('header__menu--active');
   });
 }
